ea806fa87ee1bfffc5812fc0a9a7da8b810748c3
Jagdeep Bhindar <jbhindar@indiana.edu>
2018-09-25 14:24:27 -0400

    changes before videos

---
+++ Public/API/work.php
@@ -0,0 +1,13 @@
+<?php
+class Animal
+{
+  public $type = 'Pig';
+}
+
+$wilbur = new Animal();
+echo $wilbur->type;
+
+// ?? if exists use it
+// ? : isset
+
+// $arr = [''=>'',''=>'']; or $arr = ['','','']
