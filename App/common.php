fc6cb6fca2523f8bec93233ea909032be13ef6bb
Jagdeep Bhindar <jbhindar@indiana.edu>
2018-10-02 14:00:28 -0400

    fixed error with common php file

--- App/common.php
+++ App/common.php
@@ -3,7 +3,8 @@
 chdir(__DIR__);
 set_include_path (__DIR__);
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') && stripos($_SERVER['CONTENT_TYPE'], 'application/json') !== false){
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+&& stripos($_SERVER['CONTENT_TYPE'], 'application/json') !== false){
   $_POST = json_decode(file_get_contents('php://input'), true);
 }
 
